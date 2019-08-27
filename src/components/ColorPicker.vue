c4776db1e5586890470c121010542251f69a43d6
quadro313 <quadro313@gmail.com>
2019-08-27 14:40:17 +0300

    reworked layout into components structure

---
+++ src/components/ColorPicker.vue
@@ -0,0 +1,121 @@
+<template>
+  <v-card :color="backgroundColor" raised outlined>
+    <v-container>
+      <v-row align="center" dense>
+        <v-col>
+          <v-row align="center" :color="foregroundColor" dense class="py-0 my-0">
+            <v-btn-toggle v-model="selectedColorMode" mandatory>
+              <v-btn
+                v-for="c in $_.keys(colorLimits)"
+                :key="c"
+                :value="c"
+                :color="foregroundColor"
+                :style="{backgroundColor: `${backgroundColor}`}"
+                small
+                outlined
+              >{{c}}</v-btn>
+            </v-btn-toggle>
+          </v-row>
+          <v-row v-for="(val, index) in $_.keys(colorLimits[selectedColorMode])" dense :key="index">
+            <span class="pt-1" :style="{color: `${foregroundColor}`}">{{val}}</span>
+            <v-slider
+              v-on:input="updateColor"
+              :color="foregroundColor"
+              :track-color="foregroundColor"
+              :step="colorLimits[selectedColorMode][val]['step']"
+              :min="colorLimits[selectedColorMode][val]['min']"
+              :max="colorLimits[selectedColorMode][val]['max']"
+              v-model="selectedColor[index]"
+            >
+              <template #append>
+                <input
+                  :style="{width: '75px !important',color: `${foregroundColor}`, backgroundColor: `${backgroundColor}`}"
+                  type="number"
+                  v-model="selectedColor[index]"
+                />
+              </template>
+            </v-slider>
+          </v-row>
+          <v-row dense v-if="transparency">
+              <span class="pt-1" :style="{color: `${foregroundColor}`}">A</span>
+            <v-slider
+              :color="foregroundColor"
+              :track-color="foregroundColor"
+              :step=".01"
+              :min="0"
+              :max="1"
+              v-model="alpha"
+            >
+            <template #append>
+                <input
+                  :style="{width: '75px !important',color: `${foregroundColor}`, backgroundColor: `${backgroundColor}`}"
+                  type="number"
+                  v-model="alpha"
+                />
+              </template>
+            </v-slider>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "ColorPicker",
+  props: ["backgroundColor", "foregroundColor", "transparency"],
+  data() {
+    return {
+      selectedColorMode: "RGB",
+      selectedColor: [0, 0, 0],
+      alpha: 1
+    };
+  },
+  computed: {
+    colorLimits: function() {
+      return this.$store.getters.getColorLimits;
+    }
+  },
+  watch: {
+    selectedColorMode: function(newVal) {
+      switch (newVal) {
+        case "XYZ":
+          {
+            let v = this.$store.getters.getColorAs("xyz");
+            this.selectedColor = v;
+          }
+          break;
+        case "RGB":
+          {
+            let v = this.$store.getters.getColorAs("rgb_array");
+            this.selectedColor = v;
+          }
+          break;
+        case "HSL": {
+          let v = this.$store.getters.getColorAs("hsl");
+          this.selectedColor = v;
+        }
+      }
+    }
+  },
+  methods: {
+    updateColor() {
+      let val =
+        this.selectedColorMode === "RGB"
+          ? { original: this.selectedColor, clamped: this.selectedColor }
+          : this.selectedColor;
+      this.$store.dispatch("updateColor", {
+        mode: this.selectedColorMode,
+        value: val
+      });
+    }
+  },
+  created() {
+    this.selectedColor = this.$store.getters.getColorAs("rgb_array");
+  }
+};
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,41 +3,7 @@
                  height="100%" class="py-0 my-0">
         <v-row align="stretch">
             <v-col cols="8" class="py-0 my-0">
-                <v-card :color="color" raised outlined>
-                    <v-container>
-                        <v-row align="center" dense>
-                            <v-col>
-                                <v-row align="center" :color="complementary" dense class="py-0 my-0">
-                                    <v-btn-toggle v-model="selectedColorMode" mandatory>
-                                        <v-btn v-for="c in $_.keys(colorLimits)"
-                                               :key="c"
-                                               :value="c"
-                                               :color="complementary"
-                                               :style="{backgroundColor: `${color}`}"
-                                               small outlined>
-                                            {{c}}
-                                        </v-btn>
-                                    </v-btn-toggle>
-                                </v-row>
-                                <v-row v-for="(val, index) in $_.keys(colorLimits[selectedColorMode])" dense :key="index">
-                                    <span class="pt-1" :style="{color: `${complementary}`}">{{val}}</span>
-                                    <v-slider v-on:input="updateColor"
-                                              :color="complementary"
-                                              :track-color="complementary"
-                                              :step="colorLimits[selectedColorMode][val]['step']"
-                                              :min="colorLimits[selectedColorMode][val]['min']"
-                                              :max="colorLimits[selectedColorMode][val]['max']"
-                                              v-model="selectedColor[index]">
-                                        <template #append>
-                                            <input :style="{width: '75px !important',color: `${complementary}`, backgroundColor: `${color}`}" type="number"
-                                                   v-model="selectedColor[index]"/>
-                                        </template>
-                                    </v-slider>
-                                </v-row>
-                            </v-col>
-                        </v-row>
-                    </v-container>
-                </v-card>
+                <ColorPicker :backgroundColor="color" :foregroundColor="complementary" :transparency="transparency"></ColorPicker>
             </v-col>
             <v-col class="py-0 my-0">
                 <v-card :color="color" raised outlined>
@@ -60,13 +26,17 @@
 </template>
 
 <script>
+    import ColorPicker from '../components/ColorPicker'
+    const color = require('../helpers/color.js');
+
     export default {
         name: "Home",
+        components: {
+            ColorPicker
+        },
         data() {
             return {
-                selectedColorMode: "RGB",
-                selectedColor: [0, 0, 0],
-                self: this
+                transparency: true
             }
         },
         computed: {
@@ -81,31 +51,10 @@
             }
         },
         watch: {
-          selectedColorMode: function(newVal) {
-              switch(newVal) {
-                  case 'XYZ': {
-                      let v = this.$store.getters.getColorAs('xyz');
-                      this.selectedColor = v;
-                  } break;
-                  case 'RGB': {
-                      let v = this.$store.getters.getColorAs('rgb_array');
-                      this.selectedColor = v;
-                  } break;
-                  case 'HSL': {
-                      let v = this.$store.getters.getColorAs('hsl');
-                      this.selectedColor = v;
-                  }
-              }
-          }
+          
         },
         methods: {
-            updateColor() {
-                let val = (this.selectedColorMode === 'RGB') ? {original: this.selectedColor, clamped: this.selectedColor} : this.selectedColor;
-                this.$store.dispatch("updateColor", {mode: this.selectedColorMode, value: val});
-            }
-        },
-        created() {
-            this.selectedColor = this.$store.getters.getColorAs("rgb_array");
+            
         }
     }
 </script>
